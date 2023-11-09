40290b906e1f7e690bbe2d89d23e9a476af30ae5
Bryan Matheus <bryan.profissionalll@gmail.com>
2023-11-09 17:28:13 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,12 @@ import Image from "next/image"
 
 
 import React, { useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import './styles.css';
-
-// import required modules
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
 
